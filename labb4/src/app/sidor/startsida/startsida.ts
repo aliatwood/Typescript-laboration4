93675bd3312d27db4ff67b9d871bd62b3cd927f7
aliatwood <aliatwood0104@gmail.com>
2026-05-09 19:35:37 +0200

    Skapade startsida-komponent med HTML, CSS och TypeScript

---
+++ labb4/src/app/sidor/startsida/startsida.ts
@@ -0,0 +1,53 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+import { CourseService } from '../services/course.service';
+
+@Component({
+  selector: 'app-startsida',
+  standalone: true,
+  imports: [CommonModule, FormsModule],
+  templateUrl: './startsida.component.html',
+  styleUrl: './startsida.component.css'
+})
+export class StartsidaComponent implements OnInit {
+
+  courses: any[] = [];
+  searchText: string = '';
+  sortKey: string = '';
+  sortAsc: boolean = true;
+
+  constructor(private courseService: CourseService) {}
+
+  ngOnInit(): void {
+    this.courseService.getCourses().subscribe(data => {
+      this.courses = data;
+    });
+  }
+
+  sortBy(key: string) {
+    if (this.sortKey === key) {
+      this.sortAsc = !this.sortAsc;
+    } else {
+      this.sortKey = key;
+      this.sortAsc = true;
+    }
+
+    this.courses.sort((a, b) => {
+      let valueA = a[key].toLowerCase();
+      let valueB = b[key].toLowerCase();
+
+      if (valueA < valueB) return this.sortAsc ? -1 : 1;
+      if (valueA > valueB) return this.sortAsc ? 1 : -1;
+      return 0;
+    });
+  }
+
+  filteredCourses() {
+    return this.courses.filter(course =>
+      course.code.toLowerCase().includes(this.searchText.toLowerCase()) ||
+      course.coursename.toLowerCase().includes(this.searchText.toLowerCase())
+    );
+  }
+}
